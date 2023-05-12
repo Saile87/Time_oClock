866ce69cd4464917e79027d367ac8ecb2791d2ce
Elias Breitenbach <elias.b@me.com>
2023-05-12 19:53:33 +0200

    Create a app on github

---
+++ Time_oClock/Time_oClock/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Time_oClock
+//
+//  Created by Elias Breitenbach on 12.05.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var time = Date()
+    @Environment(\.colorScheme) var colorScheme
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        Text("\(timeFormatter(time))")
+            .font(.system(size: 100))
+            .foregroundColor(timeInRange(time) ? .green : .red)
+            .background(colorScheme == .dark ? Color.black : Color.white)
+            .onReceive(timer) { input in
+                self.time = input
+            }
+    }
+    
+    func timeFormatter(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.timeZone = TimeZone.current
+        return formatter.string(from: date)
+    }
+    
+    func timeInRange(_ date: Date) -> Bool {
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        return hour >= 6 && hour <= 23
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Time_oClock/Time_oClock/Time_oClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_oClockApp.swift
+//  Time_oClock
+//
+//  Created by Elias Breitenbach on 12.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Time_oClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
